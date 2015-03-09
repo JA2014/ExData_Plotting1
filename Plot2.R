c19744f9c74385630d992013fa1337970d99ceab
JA2014 <JA2014@users.noreply.github.com>
2015-03-09 00:27:24 +0000

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,4 +1,4 @@
-## Script that construct the read data, construct plot2 and create the PNG2 file.
+## Script that construct the read data, construct plot2 and create the PNG2 file. 
 > if(!file.exists("EPC")) {dir.create("EPC")} ## if it does not exist, created the folder 'EPC' in my working directory,
 > fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip" ## Place the URL of the file to download in the variable
 > download.file(fileUrl, destfile = "./EPC/EPC.zip") ## downloads the file from the Internet in the destination file and directory
@@ -9,5 +9,6 @@
 > fevrier$Datetime <- Mydatetime  ## adds a new column 'Datetime' that has the format 'Date and Time' in the dataset
 > windows() ## launch the screen device
 > with(fevrier, plot(fevrier$Datetime, fevrier$Global_active_power, main = "", xlab = "", ylab = "Global Active Power (kilowatts)", type ="l"))## create the Plot2 with 2 variables including the annotation for axes and title, and displays it on the screen
-> dev.copy(png, file="plot2.png") ## copy the 'Plot' in a PNG file named 'plot2.png'.
+> dev.copy(png, file="plot2.png") ## copy the 'Plot' in a PNG file named 'plot2.png'. the weekday are in french because of my regional setting
 > dev.off() ## close the PNG device
+## the weekday are in french because of my regional setting. Jeu = thu, Ven = Fri, sam = Sat
