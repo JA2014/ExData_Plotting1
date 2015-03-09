266db80b0d80cf1c4f1f20e92bcac7874e8c177f
JA2014 <JA2014@users.noreply.github.com>
2015-03-09 01:53:51 +0000

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -6,17 +6,16 @@
 > library(lubridate) ## load the library for date manipulation
 > Mydatetime = dmy(fevrier$Date) + hms(fevrier$Time) ## create the variable that combines the date and time set from the columns 'Date' and 'Time'of the dataset
 > fevrier$Datetime <- Mydatetime  ## adds a new column 'Datetime' that has the format 'Date and Time' in the dataset
-> windows() ## launch the screen device
-> png()
-par(mfrow= c(2,2),mar = c(4,4,2,1), oma = c(0,0,2,0)
+> png(file = "plot4bis.png")
+> par(mfrow= c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
 > with (fevrier, {
 > plot(fevrier$Datetime, fevrier$Global_active_power, main = "", xlab = "", ylab = "Global Active Power", type ="l")
-> plot(fevrier$Datetime, fevrier$voltage, main = "", xlab = "datetime", ylab = "voltage", type ="l")
+> plot( fevrier$Datetime, fevrier$voltage, main = "", xlab = "datetime", ylab = "voltage", type ="l")
 > plot(fevrier$Datetime, fevrier$Sub_metering_1, main = "", xlab = "", ylab = "Energie sub metering", type ="l", col = "black")
 > with(fevrier, lines(fevrier$Datetime, fevrier$Sub_metering_2, main = "", xlab = "", ylab = "Energie sub metering", type ="l", col = "red"))
 > with(fevrier, lines(fevrier$Datetime, fevrier$Sub_metering_3, main = "", xlab = "", ylab = "Energie sub metering", type ="l", col = "blue"))
 > legend("topright", pch = "-", col = c("black","red","blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")) 
 > plot (fevrier$Datetime, fevrier$Global_active_power, main = "", xlab = "datetime", ylab = "voltage", type ="l")
 > }
-> dev.off()
 > )
+>dev.off()
